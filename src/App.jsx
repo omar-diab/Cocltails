3f6f06baa9c74e263e27860787f3dfe97e4bbeaa
OMAR <omar.i.s.diab@gmail.com>
2025-06-21 18:24:08 +0300

    Cocktails Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 const App = () => {
  return (
 	<main>
 		<Navbar />
 		<Hero />
+		<Cocktails />
 	</main>
  )
 }
--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,7 +1,67 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { cocktailLists, mockTailLists } from '../../constants'
+
 const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#cocktails',
+        start: 'top 30%',
+        end: 'bottom 80%',
+        scrub: true,
+      }
+    });
+
+    parallaxTimeline.from('#c-left-leaf', {
+      x: -100,
+      y: 100
+    })
+
+    parallaxTimeline.from('#c-right-leaf', {
+      x: 100,
+      y: 100
+    })
+
+  }, [])
+
+
   return (
-    <section>
-      <h1>coc</h1>
+    <section id="cocktails" className="noisy">
+      <img src="/images/cocktail-left-leaf.png" alt="left-leaf" id="c-left-leaf"/>
+      <img src="/images/cocktail-right-leaf.png" alt="right-leaf" id="c-right-leaf"/>
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails</h2>
+          <ul>
+            {cocktailLists.map((drink) => (
+              <li key={drink.id}>
+                <div className='md:me-28'>
+                    <h3>{drink.name}</h3>
+                    <p>{drink.country} | {drink.detail}</p>
+                </div>
+                <span>{drink.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="loved">
+          <h2>Most loved mocktails</h2>
+          <ul>
+            {mockTailLists.map((drink) => (
+              <li key={drink.id}>
+                <div className='md:me-28'>
+                    <h3>{drink.name}</h3>
+                    <p>{drink.country} | {drink.detail}</p>
+                </div>
+                <span>{drink.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
     </section>
   );
 };
