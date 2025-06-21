da9bd6a3337f78b29ef4c637125c32ab5d103e39
OMAR <omar.i.s.diab@gmail.com>
2025-06-21 23:27:01 +0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 const App = () => {
  return (
@@ -19,6 +20,7 @@ const App = () => {
 		<About />
 		<Art />
 		<Menu />
+		<Contact />
 	</main>
  )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,89 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { openingHours, socials } from "../../constants";
+import { SplitText } from "gsap/all";
 
 const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+    const timeLine = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeLine
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to("#f-left-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      }, '<');
+  }, []);
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer id="contact">
+      <img
+        src="/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+      <img
+        src="/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+      <div className="content">
+        <h2>Where To Find Us</h2>
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>456, Raq Blvd. #404, Los Angles, CA 90210</p>
+        </div>
+        <div>
+          <h3>Contact Us</h3>
+          <p>(555)987-6543</p>
+          <p>hello@cockatils.com</p>
+        </div>
+        <div>
+          <h3>Open Every Day</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day}: {time.time}
+            </p>
+          ))}
+        </div>
+        <div>
+          <h3>Socials</h3>
+          <div className="flex-center gap-5">
+            {socials.map((social) => (
+              <a
+                key={social.name}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={social.name}
+              >
+                <img src={social.icon} alt={social.name} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
