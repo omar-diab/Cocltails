1d11e65df6648cb5662fd484dc630e7db60757b7
OMAR <omar.i.s.diab@gmail.com>
2025-06-21 22:24:06 +0300

    Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 const App = () => {
  return (
@@ -15,6 +16,7 @@ const App = () => {
 		<Hero />
 		<Cocktails />
 		<About />
+		<Art />
 	</main>
  )
 }
