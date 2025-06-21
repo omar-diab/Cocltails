4f453b68a5ed32cc631fb93e62d5c4edd226098c
OMAR <omar.i.s.diab@gmail.com>
2025-06-21 16:33:06 +0300

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
  return (
 	<main>
-
+		<Navbar />
+		<Hero />
 	</main>
  )
 }
